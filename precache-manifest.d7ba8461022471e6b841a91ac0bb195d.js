21ef5524c8a097f7a490194f6d8a0b4671a86ac3
ravishberi <ravishberi@yahoo.com>
2020-03-02 13:30:02 -0500

    Updates

---
+++ precache-manifest.d7ba8461022471e6b841a91ac0bb195d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b56d92ff2b0f1fb7b3bf46d4750eafe3",
+    "url": "/BookExplorer/index.html"
+  },
+  {
+    "revision": "dab1b6e9306e4322ef39",
+    "url": "/BookExplorer/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "bdf271a6818cc75b0b42",
+    "url": "/BookExplorer/static/js/2.039b48f2.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/BookExplorer/static/js/2.039b48f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dab1b6e9306e4322ef39",
+    "url": "/BookExplorer/static/js/main.80870251.chunk.js"
+  },
+  {
+    "revision": "070fefd7340993f3dbec",
+    "url": "/BookExplorer/static/js/runtime-main.e3e7fc68.js"
+  }
+]);
